023b74b77617034706c336f0819aa5fe829cb9a7
Sohil007 <sohil.mansuri@in.musafir.com>
2024-09-06 18:23:40 +0530

    architecture test added

---
+++ Musafir.Test.UnitTest/ArchitectureTests/ArchitectureTests.cs
@@ -0,0 +1,27 @@
+﻿using NetArchTest.Rules;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using Musafir.AmaduesAPI;
+
+namespace Musafir.Test.UnitTest.ArchitectureTests
+{
+    [TestFixture]
+    public class ArchitectureTests
+    {
+        [Test]
+        public void Should_Not_Have_Reference_Of_UnitTest_Porject()
+        {
+            var webapinamesapce = "Musafir.AmaduesAPI";
+            var unitTestnamespace = "Musafir.Test.UnitTest";
+
+            var result = Types.InAssembly(typeof(Program).Assembly)
+                .That()
+                .ResideInNamespace(webapinamesapce)
+                .ShouldNot()
+                .HaveDependencyOn(unitTestnamespace)
+                .GetResult();
+
+            ClassicAssert.IsTrue(result.IsSuccessful);
+        }
+    }
+}
